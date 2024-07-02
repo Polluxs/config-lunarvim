80ee5a80dfddd197a676d18dda492a3fe8a9de1c
Polluxs <thomasdorissen@gmail.com>
2024-07-02 17:57:23 +0200

    Add golang debugging and rainbow brackets

--- config.lua
+++ config.lua
@@ -57,14 +57,85 @@ lvim.builtin.which_key.mappings["sT"] = {
 -- ## User plugins
 -- ########################
 --
+--
+lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.rainbow.extended_mode = true
 lvim.plugins = {
-  -- this plugin highlights todos & fixme etc
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+  -- NOTE: highlight plugin
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
+
+  -- NOTE: Debugging
+  "mfussenegger/nvim-dap",
+  "leoluz/nvim-dap-go",
+  "mrjones2014/nvim-ts-rainbow",
 }
 
+-- NOTE: Debugging
+lvim.builtin.dap.active = true
+local dap = require("dap")
+
+dap.adapters.go = function(callback, _)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { "dap", "-l", "127.0.0.1:" .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print("dlv exited with code", code)
+    end
+  end)
+  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  -- Wait for delve to start
+  vim.defer_fn(function()
+    callback { type = "server", host = "127.0.0.1", port = port }
+  end, 100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  -- {
+  --   type = "go",
+  --   name = "Debug test",   -- configuration for debugging test files
+  --   request = "launch",
+  --   mode = "test",
+  --   program = "${file}",
+  -- },
+  -- -- works with go.mod packages and sub packages
+  -- {
+  --   type = "go",
+  --   name = "Debug test (go.mod)",
+  --   request = "launch",
+  --   mode = "test",
+  --   program = "./${relativeFileDirname}",
+  -- },
+}
